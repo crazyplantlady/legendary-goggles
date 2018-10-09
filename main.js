e21ba12fd0fe4f0e1b4392ccea66b5d3ae7b4c97
0 <aesa64@gmail.com>
2018-10-09 22:51:38 +0000

    initial commit

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var nr6= "True, thvi <about kalli> er sama og <Kalli er godur strakur> '===' thy
 var nr7= "thetta er false, thvi badar fullyrdingarnar geta ekki verid rettar og ef onnur er false tha er utkoman false, thvi thad er notad ampersand a milli, ef thad vaeri notad <or ||> thad vaeri breytan rett.";
 var nr8= "!0 er true, og ofugt vid thad er false svo not not true er false. !-2 er false, svo !!-2 verdur true. <!gæsalappir> er true svo <!!gæsalappir> verdur false. Kalli er true, !kalli er false og !!kalli verdur tha true";
 var nr9= "svarid er true, thvi 5 er minna en 9 og meira en 2";
+var nr10= "nr1 zoey=knight. nr2 peggy=knight. nr3 Zippy=knave"
 
 
 
