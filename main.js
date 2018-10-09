1fbb23cafe3ff2395f623f90f7dc076933cc5d97
0 <aesa64@gmail.com>
2018-10-09 22:17:04 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var nr1= "halló" + " heimur";
+var nr2= 5+8-2/2*2;
+var nr3= !(true) && true || false;
+var nr4= (5<10 && 11>5) || 5===3;
+var nr5= (3>=3)!==(6<=9);
+var nr6= "True, thvi <about kalli> er sama og <Kalli er godur strakur> '===' thydir true";
+var nr7= "thetta er false, thvi badar fullyrdingarnar geta ekki verid rettar og ef onnur er false tha er utkoman false, thvi thad er notad ampersand a milli, ef thad vaeri notad <or ||> thad vaeri breytan rett.";
+var nr8= "!0 er true, og ofugt vid thad er false svo not not true er false. !-2 er false, svo !!-2 verdur true. <!gæsalappir> er true svo <!!gæsalappir> verdur false. Kalli er true, !kalli er false og !!kalli verdur tha true";
+var nr9= "svarid er true, thvi 5 er minna en 9 og meira en 2";
+
+
+
